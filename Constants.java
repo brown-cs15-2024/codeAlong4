559475c07c3f45f629e3e73d1bdca2a89f597b7f
Ilan Brauns <113201070+ilanbrauns@users.noreply.github.com>
2024-06-28 14:29:23 -0400

    Initial commit

---
+++ Constants.java
@@ -0,0 +1,10 @@
+package codeAlong3;
+
+public class Constants {
+
+    public static final double APP_WIDTH = 300;
+    public static final double APP_HEIGHT = 300;
+
+    //TODO Add constants for your Alien here!
+
+}
---
+++ PaneOrganizer.java
@@ -0,0 +1,28 @@
+package codeAlong3;
+
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+public class PaneOrganizer {
+
+    private Pane rootPane;
+    //TODO Put any instance variables you need here!
+
+
+    public PaneOrganizer() {
+        this.rootPane = new BorderPane();
+        //TODO Set up panes and call any helper methods
+
+    }
+
+    public Pane getRoot() {
+        return this.rootPane;
+    }
+
+    //TODO Fill out this method to create your buttons!
+    private void setUpButtons(HBox buttonPane) {
+
+    }
+
+}
